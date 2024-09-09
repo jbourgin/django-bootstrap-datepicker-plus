f3516870218af53da3e9a776a4196c9882aa6ea0
Jessica Bourgin <jessica.bourgin@gmail.com>
2024-09-09 13:36:02 +0200

    add days_forbidden option

--- bootstrap_datepicker_plus/_base.py
+++ bootstrap_datepicker_plus/_base.py
@@ -27,6 +27,7 @@ class BasePickerInput(DateTimeBaseInput):
         "showClose": True,
         "showClear": True,
         "showTodayButton": True,
+        "days_forbidden": [],
     }
 
     # source: https://github.com/tutorcruncher/django-bootstrap3-datetimepicker
